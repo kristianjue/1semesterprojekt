2ad254d7fa02de7582073a7b8cb76dc72d4822b3
Fawad Nassery <fawad-nassery@hotmail.com>
2023-11-16 11:15:23 +0100

    Co-authored-by: Sandra <sandra8906@users.noreply.github.com>
    tilføjelse af piechart

--- script.js
+++ script.js
@@ -39,3 +39,61 @@
             .style('transform','scale(1.0)');    
         })
     })
+
+
+
+
+
+
+
+
+
+
+    // PIE CHART
+    var w = 200;
+    var h = 200;
+
+    var dataset = [ 5, 10, 20, 45, 6, 25 ];
+
+    var outerRadius = w / 2;
+    var innerRadius = 0;
+    var arc = d3.arc()
+          .innerRadius(innerRadius)
+          .outerRadius(outerRadius);
+    
+    var pie = d3.pie();
+    
+    //Easy colors accessible via a 10-step ordinal scale
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    //Create SVG element
+    var svg = d3.select("body")
+          .append("svg")
+          .attr("width", w)
+          .attr("height", h);
+    
+    //Set up groups
+    var arcs = svg.selectAll("g.arc")
+            .data(pie(dataset))
+            .enter()
+            .append("g")
+            .attr("class", "arc")
+            .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
+    
+    //Draw arc paths
+    arcs.append("path")
+        .attr("fill", function(d, i) {
+          return color(i);
+        })
+        .attr("d", arc);
+    
+    //Labels
+    arcs.append("text")
+        .attr("transform", function(d) {
+          return "translate(" + arc.centroid(d) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+          return d.value;
+        });
+
