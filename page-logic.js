c0d1eee1ee54ce1b6d8d9935c3f87d47042128a7
kjuelsgaard <kristian@juelsgaard.dk>
2023-12-11 11:42:17 +0100

    Added sound files

--- page-logic.js
+++ page-logic.js
@@ -7,12 +7,11 @@ new fullpage("#fullpage", {
   },
 });
 
-const slide1 = new Audio("page0.mp3");
-const slide2 = new Audio("ambient-piano-logo-165357.mp3");
-const sound3 = new Audio("mixkit-arcade-retro-game-over-213.wav");
-const sound4 = new Audio("mixkit-arcade-retro-game-over-213.wav");
-const sound5 = new Audio("mixkit-arcade-retro-game-over-213.wav");
-const sound6 = new Audio("mixkit-arcade-retro-game-over-213.wav");
+const slide1 = new Audio("/sound_files/Introduction.mp3");
+const slide2 = new Audio("/sound_files/development_of_vegetarians.mp3");
+const sound3 = new Audio("/sound_files/Co2_meals.mp3");
+const sound4 = new Audio("/sound_files/BEV_vs_ICE_graph.mp3");
+const sound5 = new Audio("/sound_files/BEV_vs_ICE_co2_compareson.mp3");
 
 let speech = document.querySelector(".speech-bubble");
 
@@ -27,6 +26,12 @@ function toggleSpeechBubble() {
     slide1.play();
   } else if (ActiveSectionNumber === "2") {
     slide2.play();
+  } else if (ActiveSectionNumber === "3") {
+    slide3.play();
+  } else if (ActiveSectionNumber === "4") {
+    slide4.play();
+  } else if (ActiveSectionNumber === "5") {
+    slide5.play();
   }
 }
 
@@ -36,5 +41,4 @@ function disableAudio() {
   sound3.pause();
   sound4.pause();
   sound5.pause();
-  sound6.pause();
 }
